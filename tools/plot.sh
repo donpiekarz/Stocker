d0e5c1a6ec05f5a91fd33ec4196ea37ad8802118
Mateusz Lis <mateusz.lis@gmail.com>
2011-12-23 11:15:18 +0100

    [FEAT] Implemented plotting of moments, when simulated Investor bought or sold shares

--- tools/plot.sh
+++ tools/plot.sh
@@ -7,12 +7,18 @@ set timefmt "%Y%m%d"
 set xdata time
 set multiplot
 set origin 0.0, 0.2
-set size 1.0, 0.8
-plot "$1" u 2:3 w l
+set size 1.0, 0.5
+plot "$1" u 2:3 w l t "Price"
 set origin 0.03, 0.0
 set size 0.97, 0.2
 unset ytics
 unset xtics
-plot "$1" u 2:7 w boxes
+plot "$1" u 2:7 w boxes t "Volume"
+set origin 0.0, 0.68
+set size 1.0, 0.34
+set ytics 30
+unset xtics
+set title "Simulation results with $1"
+plot "$1" u 2:8 w boxes t "Buy", "$1" u 2:(-\$9) w boxes t "Sell"
 EOF
 epstopdf stocker.eps
