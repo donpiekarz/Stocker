c7e7d51575c57874aec465f01d538ce55829ff1b
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2011-12-23 12:19:27 +0100

    [FIX] Moved TransactionsDownloader to special directory

---
+++ TransactionsDownloader/config.py
@@ -0,0 +1,15 @@
+
+# global
+configCompanies = [ "cia", "cdc" ]
+
+
+# transactionsDownloader
+
+configTransactionsCompaniesIds = { "cia": 6816, "cdc":6093 }
+
+configTransactionsDir = "/tmp/stocker/data/transactions"
+configTransactionsURL = "http://gielda.onet.pl/18648,101,1,%(id)s,1,profile-transakcje-csv?lim=0&date=%(date)s"
+
+
+
+
--- config.py
+++ config.py
@@ -3,13 +3,3 @@
 configCompanies = [ "cia", "cdc" ]
 
 
-# transactionsDownloader
-
-configTransactionsCompaniesIds = { "cia": 6816, "cdc":6093 }
-
-configTransactionsDir = "/tmp/stocker/data/transactions"
-configTransactionsURL = "http://gielda.onet.pl/18648,101,1,%(id)s,1,profile-transakcje-csv?lim=0&date=%(date)s"
-
-
-
-
