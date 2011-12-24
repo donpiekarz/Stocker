8821d2f248cf9f19213c8a56f2bf4d843665bf4d
Bom Pom <donpiekarz@gmail.com>
2011-12-24 21:53:54 +0100

    [FEAT] Added function getLogger

---
+++ utils.py
@@ -0,0 +1,19 @@
+
+# global
+import logging
+import os
+
+# project
+import config
+
+
+
+def getLogger(app, level=config.logLevel):
+	filename = app + '.log'
+        path = os.path.join(config.logDir, filename)
+
+	logging.basicConfig(filename=path, level=level, format=config.logFormat)
+        logger = logging.getLogger(app)
+
+        return logger
+
