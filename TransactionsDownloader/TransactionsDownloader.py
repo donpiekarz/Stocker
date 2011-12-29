bbda80c92dfced06798af1fec9ebcec589461fe7
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2011-12-29 23:09:33 +0100

    [FEAT][TD] Added OptionParser. It can download transactions log for given date

--- TransactionsDownloader/TransactionsDownloader.py
+++ TransactionsDownloader/TransactionsDownloader.py
@@ -3,7 +3,9 @@
 import sys
 import os
 import urllib2
-from time import gmtime, strftime
+import time 
+from optparse import OptionParser
+import re
 
 # project
 from config import *
@@ -20,7 +22,9 @@ def checkDirs():
 
 def main():
     print "TransactionsDownloader starts ..."
-    args = { "date" : strftime("%Y-%m-%d", gmtime()) }
+
+    options = parseCommandLine()
+    args = { "date" : options.date }
     
     checkDirs()
 
@@ -43,6 +47,22 @@ def main():
     print "all done"
 
 
+
+def parseCommandLine():
+    parser = OptionParser()
+
+    parser.add_option("-d", "--date", type="string", dest="date", default="", help="Download transactions for date", metavar="DATE")
+
+
+    (options, _) = parser.parse_args()
+
+    if options.date == "":
+        options.date = time.strftime("%Y-%m-%d",  time.gmtime())
+
+    if not re.match("\d{4}-\d{2}-\d{2}", options.date):
+        parser.error("Option --date should be %Y-%m-%d e.g. 2011-12-29")
+
+    return options
 
 
 if __name__ == "__main__":
