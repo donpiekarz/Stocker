920e4c37ef7b6f76d735ecdb94d829a868231495
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-15 13:36:01 +0100

    Added company multiplexer in GeneticInvestor

--- bin/SEP.py
+++ bin/SEP.py
@@ -9,8 +9,11 @@ from stocker.common.stream import Stream
 def main():
     print "hello!"
     
-    dirname_in = "C:\\code\\stoker_data"
-    filename_out = os.path.join(dirname_in, 'stream1.stm')
+    #dirname_in = "C:\\code\\stoker_data\\small"
+    #dirname_in = "C:\\code\\stoker_data\\large"
+    #dirname_in = "C:\\code\\stoker_data\\amica_cia\\small"
+    dirname_in = "C:\\code\\stoker_data\\amica_cia\\large"
+    filename_out = os.path.join(dirname_in, 'large.stm')
     
     stream = Stream()
     stream.walk(dirname_in)
