425b14958970a25246cabad9da01191c2fa85c11
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-20 22:42:52 +0100

    Corrected conditions in transaction processing method in stockbroker

--- bin/SEP.py
+++ bin/SEP.py
@@ -1,23 +1,21 @@
-
-
 """Stocker ETL Processor"""
 
 import os
 
-from stocker.common.stream import Stream 
+from stocker.common.stream import Stream
 
 def main():
     print "hello!"
-    
+
     #dirname_in = "C:\\code\\stoker_data\\small"
     #dirname_in = "C:\\code\\stoker_data\\large"
     #dirname_in = "C:\\code\\stoker_data\\amica_cia\\small"
-    dirname_in = "C:\\code\\stoker_data\\amica_cia\\large"
-    filename_out = os.path.join(dirname_in, 'large.stm')
-    
+    dirname_in = "C:\\code\\stocker_data\\small"
+    filename_out = os.path.join(dirname_in, 'small.stm')
+
     stream = Stream()
     stream.walk(dirname_in)
-    
+
     stream.save(filename_out)
 
 
--- stocker/SSP/stockbroker.py
+++ stocker/SSP/stockbroker.py
@@ -1,77 +1,71 @@
-
-
-import sys
 import collections
-import decimal
 
 from stocker.common.orders import OrderBuy, OrderSell
 from stocker.common.events import EventStockTransaction
 
 from stocker.SSP.investor import Investor
 
-class Account ( object ):
+class Account(object):
     cash = 0
     cash_blocked = 0
     shares = collections.defaultdict(int)
     shares_blocked = collections.defaultdict(int)
 
 
-class Stockbroker( object ):
-    
+class Stockbroker(object):
     stock = None
     investors = []
     accounts = {}
     orders = {}
-    
+
     def __init__(self, stock):
         self.stock = stock
 
     @staticmethod
     def create_from_config(stock, stockbroker_tree):
         stockbroker = Stockbroker(stock)
-        
+
         for inv_tree in stockbroker_tree.getElementsByTagName("Investor"):
             inv = Investor.create_from_config(stockbroker, inv_tree)
-            
+
             stockbroker.investors.append(inv)
             stockbroker.accounts[inv] = Account()
-        
+
         return stockbroker
 
     def new_order(self, order, investor):
         order.investor = investor
-        
+
         account = self.accounts[order.investor]
-        
+
         if isinstance(order, OrderBuy):
             value = order.amount * order.limit_price
             if account.cash < value:
                 raise self.NotEnoughCashError
-             
+
             account.cash -= value
             account.cash_blocked += value
-            
+
             self.stock.new_order(order, self)
-            
+
         elif isinstance(order, OrderSell):
             if account.shares[order.company_id] < order.amount:
                 raise self.NotEnoughSharesError
-            
+
             account.shares[order.company_id] -= order.amount
             account.shares_blocked[order.company_id] += order.amount
-            
+
             self.stock.new_order(order, self)
-            
-    
+
+
     def del_order(self, order):
         pass
-    
+
     def process(self, event):
-        
-        if isinstance(event, EventStockTransaction): 
-            
-            if event.buy_order.investor or event.sell_order.investor: self.process_transaction(event)
-            else: 
+        if isinstance(event, EventStockTransaction):
+            if hasattr(event.buy_order, 'investor') or hasattr(event.sell_order, 'investor'): self.process_transaction(
+                event)
+            else:
                 for inv in self.investors:
                     inv.process(event)
 
@@ -80,33 +74,37 @@ class Stockbroker( object ):
                 inv.process(event)
 
     def process_transaction(self, event):
-        if event.buy_order.investor:
+        if hasattr(event.buy_order, 'investor') and event.buy_order.investor:
             order = event.buy_order
-        elif event.sell_order.investor:
+        elif hasattr(event.sell_order, 'investor') and event.sell_order.investor:
             order = event.sell_order
+        else:
+            return
+
         account = self.accounts[order.investor]
         value = order.amount * order.limit_price
-        
+
         if isinstance(order, OrderBuy):
             account.cash_blocked -= value
             account.shares[order.company_id] += order.amount
-            
+
         elif isinstance(order, OrderSell):
             account.cash += value
             account.shares_blocked[order.company_id] -= order.amount
-        
-        event.order.investor.process(event)
-            
+
+        order.investor.process(event)
+
 
     def add_investor(self, investor):
         self.investors.append(investor)
         self.accounts[investor] = Account()
-        
+
     def transfer_cash(self, owner, cash):
         self.accounts[owner].cash += cash
-        
+
     class NotEnoughCashError(Exception):
         pass
+
     class NotEnoughSharesError(Exception):
         pass
         
