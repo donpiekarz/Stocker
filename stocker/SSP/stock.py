f93781fa827b709ece2df913858fd18b0eb9bd24
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-21 19:30:00 +0100

    Added method next_event into Stream class

--- stocker/SSP/stock.py
+++ stocker/SSP/stock.py
@@ -1,4 +1,3 @@
-
 import collections
 
 from stocker.common.stream import Stream
@@ -9,95 +8,90 @@ from stocker.SSP.stockbroker import Stockbroker
 
 
 class Stock(object):
-    
     stream = None
     stockbrokers = []
     companies = collections.defaultdict(dict)
-    
+
     def __init__(self):
         pass
-    
+
     @staticmethod
     def create_from_config(stock_tree):
         stock = Stock()
-        
+
         stock.stream = Stream.load(stock_tree.getElementsByTagName("stream")[0].firstChild.nodeValue)
-        
+
         for sb_tree in stock_tree.getElementsByTagName("Stockbroker"):
             sb = Stockbroker.create_from_config(stock, sb_tree)
             stock.stockbrokers.append(sb)
-            
+
         return stock
-    
+
     def new_order(self, order, stockbroker):
         order.stockbroker = stockbroker
 
         if not self.companies[order.company_id]:
             self.companies[order.company_id]['sell'] = []
             self.companies[order.company_id]['buy'] = []
-        
+
         if isinstance(order, OrderBuy):
             self.companies[order.company_id]['buy'].append(order)
-            
+
         elif isinstance(order, OrderSell):
             self.companies[order.company_id]['sell'].append(order)
-        
+
         pass
-    
+
     def del_order(self, order, stockbroker):
         order.stockbroker = stockbroker
-        
+
         if not self.companies[order.company_id]:
             self.companies[order.company_id]['sell'] = []
             self.companies[order.company_id]['buy'] = []
-        
+
         if isinstance(order, OrderBuy):
             self.companies[order.company_id]['buy'].remove(order)
-            
+
         elif isinstance(order, OrderSell):
             self.companies[order.company_id]['sell'].remove(order)
-        
+
         pass
-    
+
     def simulate(self):
-        
-        for event in self.stream.history:
-            
+        for event in self.stream.next_event():
             if isinstance(event, (EventStockOpen, EventStockClose)):
                 for sb in self.stockbrokers:
                     sb.process(event)
-                    
+
             elif isinstance(event, (EventStreamNew, )):
                 order = event.order
                 order.owner = None
                 self.new_order(order, None)
-                
+
                 for sb in self.stockbrokers:
                     sb.process(EventStockOrderNew(event.timestamp, order))
-                    
+
                 self.match_orders()
-        
+
     def match_orders(self):
-        
         event_list = []
-        
+
         for key in self.companies.keys():
             buy_list = self.companies[key]['buy']
             sell_list = self.companies[key]['sell']
-            
+
             sell_list.sort(key=lambda order: order.limit_price)
             buy_list.sort(key=lambda order: order.limit_price, reverse=True)
-            
+
             while len(buy_list) > 0 and len(sell_list) > 0:
                 if buy_list[0].limit_price >= sell_list[0].limit_price:
                     buy_order = buy_list.pop(0)
                     sell_order = sell_list.pop(0)
-                    
+
                     event_list.append(EventStockTransaction(1, buy_order, sell_order))
                 else:
                     break
-                    
-                    
+
         for event in event_list:
             if event.buy_order.stockbroker is None and event.sell_order.stockbroker is None:
                 for sb in self.stockbrokers:
--- stocker/common/stream.py
+++ stocker/common/stream.py
@@ -1,5 +1,3 @@
-
-
 import cPickle
 import csv
 import datetime
@@ -7,13 +5,16 @@ import decimal
 import os
 
 from stocker.common.events import EventStreamNew, EventStockOpen, EventStockClose
-from stocker.common.orders import Order, OrderBuy, OrderSell
+from stocker.common.orders import  OrderBuy, OrderSell
 
 class Stream:
-    
     def __init__(self):
         self.history = []
-    
+
+    def next_event(self):
+        for event in self.history:
+            yield event
+
     def add_file(self, company_id, date, filename_in):
         with open(filename_in, 'r') as f:
             for row in csv.reader(f, delimiter=';'):
@@ -25,11 +26,13 @@ class Stream:
                         limit_price = decimal.Decimal(row[1].replace(',', '.'))
                         timestamp = datetime.datetime.strptime("%s %s" % (date, row[0]), "%Y-%m-%d %H:%M:%S")
                         expiration_date = timestamp + datetime.timedelta(days=1)
-                        self.history.append(EventStreamNew(timestamp, OrderBuy(company_id, amount, limit_price, expiration_date)))
-                        self.history.append(EventStreamNew(timestamp, OrderSell(company_id, amount, limit_price, expiration_date)))
+                        self.history.append(
+                            EventStreamNew(timestamp, OrderBuy(company_id, amount, limit_price, expiration_date)))
+                        self.history.append(
+                            EventStreamNew(timestamp, OrderSell(company_id, amount, limit_price, expiration_date)))
                 except IndexError:
                     pass
-                
+
     def walk(self, dir_path):
         for dir in os.walk(dir_path).next()[1]:
             for file in os.walk(os.path.join(dir_path, dir)).next()[2]:
@@ -39,14 +42,14 @@ class Stream:
 
     def add_stock_events(self):
         dates = {}
-        
+
         for e in self.history:
             dates[e.timestamp.date()] = 1
-            
+
         for d in dates.keys():
             self.history.append(EventStockOpen(datetime.datetime.combine(d, datetime.time(9, 0))))
             self.history.append(EventStockClose(datetime.datetime.combine(d, datetime.time(18, 0))))
-        
+
 
     def save(self, filename_out):
         self.add_stock_events()
