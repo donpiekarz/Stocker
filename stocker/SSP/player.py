5b7048233e7f408d4e91e2dcb7154c603b45562b
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-22 11:06:29 +0100

    Added printing simple summary in Player class

--- stocker/SSP/player.py
+++ stocker/SSP/player.py
@@ -1,21 +1,27 @@
 from xml.dom import minidom
+import datetime
 
 from stocker.SSP.stock import Stock
 
-class Player ( object ):
-    
+class Player(object):
     @staticmethod
     def main(config_file):
         print "hello!"
-        
+
         main_tree = minidom.parse(config_file)
-        
+
         stock_tree = main_tree.childNodes[0].getElementsByTagName("Stock")[0]
         stock = Stock.create_from_config(stock_tree)
-        
+
+        start_time = datetime.datetime.now()
         stock.simulate()
-    
-    
+        stop_time = datetime.datetime.now()
+
+        # summary
+        print "End of simulation!"
+        print "Simulation lasted: %s" % (stop_time - start_time)
+
+
 if __name__ == "__main__":
     conf = "ssp.xml"
     Player.main(conf)
