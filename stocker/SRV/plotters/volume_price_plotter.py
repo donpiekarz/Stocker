be7abe617ae286797cf4a8515ac1abf09fb4a84c
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-25 18:10:24 +0100

    [SRV] Added VolumePricePlotter and implemented first main for SRV

---
+++ stocker/SRV/plotters/volume_price_plotter.py
@@ -0,0 +1,41 @@
+import matplotlib as mpl
+
+from stocker.SRV.plotters.base_plotter import BasePlotter
+from stocker.common.events import EventInvestorReportOrderRealized
+from stocker.common.orders import OrderBuy, OrderSell
+
+class VolumePricePlotter(BasePlotter):
+    def __init__(self):
+        self.x_list = [] # x axis
+        self.y_list = [] # y axis
+        self.s_list = [] # size
+        self.c_list = [] # color
+
+    def __process_order_realized(self, event):
+        x = mpl.dates.date2num(event.timestamp)
+        y = event.order.limit_price
+        s = event.order.amount * 10
+
+        if isinstance(event.order, OrderBuy):
+            c = 'r'
+        elif isinstance(event.order, OrderSell):
+            c = 'b'
+        else:
+            c = 'y'
+
+        self.x_list.append(x)
+        self.y_list.append(y)
+        self.s_list.append(s)
+        self.c_list.append(c)
+
+    def process(self, event):
+        if isinstance(event, EventInvestorReportOrderRealized):
+            self.__process_order_realized(event)
+
+    def draw_plot(self, fig):
+        ax = fig.add_subplot(111)
+        ax.scatter(self.x_list, self.y_list, c=self.c_list, s=self.s_list)
+        ax.xaxis_date()
+        ax.grid(True)
+        fig.autofmt_xdate()
+
--- stocker/SRV/visualizer.py
+++ stocker/SRV/visualizer.py
@@ -1,5 +1,23 @@
+import matplotlib.pyplot as plt
+
+from stocker.SRV.plotters.volume_price_plotter import VolumePricePlotter
+from stocker.common.utils import Stream
+
 class Visualizer(object):
     @staticmethod
     def main(reports):
-        pass
+        report = reports[0]
+
+        plotter = VolumePricePlotter()
+
+        for event in Stream.next_event(report):
+            plotter.process(event)
+
+        fig = plt.figure()
+
+        plotter.draw_plot(fig)
+
+        plt.show()
+
+
 
