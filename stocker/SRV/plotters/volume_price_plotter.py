325846a5217cc4073cda768f399adeda4189613c
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-27 21:31:29 +0100

    [SRV][VolumePricePlotter] Added support for multiple companies in report file

--- stocker/SRV/plotters/volume_price_plotter.py
+++ stocker/SRV/plotters/volume_price_plotter.py
@@ -1,3 +1,4 @@
+import collections
 import matplotlib as mpl
 
 from stocker.SRV.plotters.base_plotter import BasePlotter
@@ -6,15 +7,20 @@ from stocker.common.orders import OrderBuy, OrderSell
 
 class VolumePricePlotter(BasePlotter):
     def __init__(self):
-        self.x_list = [] # x axis
-        self.y_list = [] # y axis
-        self.s_list = [] # size
-        self.c_list = [] # color
+        self.companies = {}
+
+        self.x_list = collections.defaultdict(list) # x axis
+        self.y_list = collections.defaultdict(list) # y axis
+        self.s_list = collections.defaultdict(list) # size
+        self.c_list = collections.defaultdict(list) # color
 
     def __process_order_realized(self, event):
+        company = event.order.company_id
+        self.companies[company] = 1
+
         x = mpl.dates.date2num(event.timestamp)
         y = event.order.limit_price
-        s = event.order.amount * 10
+        s = event.order.amount * float(event.order.limit_price) / 10
 
         if isinstance(event.order, OrderBuy):
             c = 'r'
@@ -23,24 +29,32 @@ class VolumePricePlotter(BasePlotter):
         else:
             c = 'y'
 
-        self.x_list.append(x)
-        self.y_list.append(y)
-        self.s_list.append(s)
-        self.c_list.append(c)
+        self.x_list[company].append(x)
+        self.y_list[company].append(y)
+        self.s_list[company].append(s)
+        self.c_list[company].append(c)
 
     def process(self, event):
         if isinstance(event, EventInvestorReportOrderRealized):
             self.__process_order_realized(event)
 
     def draw_plot(self, fig):
-        ax = fig.add_subplot(111)
-        ax.scatter(self.x_list, self.y_list, c=self.c_list, s=self.s_list)
-        ax.xaxis_date()
-        ax.grid(True)
+        i = 0
+        size = len(self.companies.keys())
+        for company in self.companies.keys():
+            ax = fig.add_subplot(size, 1, i)
+            ax.grid(True)
+            ax.set_title('Company: %s' % company)
+            ax.set_xlabel("Time")
+            ax.set_ylabel("Price")
+            ax.scatter(self.x_list[company], self.y_list[company], c=self.c_list[company], s=self.s_list[company])
+            ax.xaxis_date()
 
-        ob = mpl.patches.Circle((0, 0), fc="r")
-        os = mpl.patches.Circle((0, 0), fc="b")
-        ax.legend([ob, os], ["Order Buy", "Order Sell"], loc=2)
+            ob = mpl.patches.Circle((0, 0), fc="r")
+            os = mpl.patches.Circle((0, 0), fc="b")
+            ax.legend([ob, os], ["Order Buy", "Order Sell"], loc=2)
 
-        fig.autofmt_xdate()
+            fig.autofmt_xdate()
+
+            i += 1
 
