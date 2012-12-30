7dfc97467e7ee6a6d3397d66706cd967279bda3d
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2012-12-30 12:53:35 +0100

    [SSP][Investors] Added GreedyInvestor

---
+++ stocker/SSP/investors/greedy_investor.py
@@ -0,0 +1,38 @@
+import collections
+import decimal
+import random
+
+from stocker.SSP.investors.base_investor import BaseInvestor
+
+class GreedyInvestor(BaseInvestor):
+    init_cash = 1000
+    buy_threshold = 0.5
+    roi = 1.05
+
+    price = collections.defaultdict(decimal.Decimal)
+    shares = collections.defaultdict(int)
+
+    def prepare(self):
+        self.stockbroker.transfer_cash(self, self.init_cash)
+
+    def _process_order_buy(self, event):
+        if random.random() > self.buy_threshold:
+            self._buy(event.order)
+
+    def _process_order_sell(self, event):
+        company_id = event.order.company_id
+        if event.order.limit_price > self.price[company_id] * decimal.Decimal(self.roi) and self.shares[company_id]:
+            self._sell(event.order)
+
+    def _process_bought(self, event):
+        company_id = event.buy_order.company_id
+
+        self.price[company_id] = ((self.price[company_id] * self.shares[company_id]) + (
+            event.buy_order.limit_price * event.buy_order.amount)) / (self.shares[company_id] + event.buy_order.amount)
+        self.shares[company_id] += event.buy_order.amount
+
+    def _process_sold(self, event):
+        company_id = event.buy_order.company_id
+        self.shares[company_id] -= event.sell_order.amount
+
+
