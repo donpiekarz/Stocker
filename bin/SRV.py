ae32673f73f62f87042163150d8c2ec1c7912b58
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2014-04-06 19:13:44 +0200

    [SRV] fix after remove unused imposts,  in args passing

--- bin/SRV.py
+++ bin/SRV.py
@@ -4,22 +4,24 @@ __additional_help__ = """Visualizes results of Stocker simulation"""
 
 #local imports
 from stocker.common import basic_program_structure
-from stocker.SEP.processor import Processor
 from stocker.SRV.visualizer import Visualizer
+
 
 @basic_program_structure.parse_command_line(__desc__, __additional_help__)
 def add_command_line_params(parser):
     """Sets up command line arguments and parses them"""
-    parser.add_argument("stream_file", help="path to simulation stream file")
-    parser.add_argument("--data_visualizer", help="data visualizer"\
-            , default="stocker.SRV.plotters.volume_price_plotter.VolumePricePlotter")
+    parser.add_argument("--stream_file", help="path to simulation stream file")
+    parser.add_argument("--data_visualizer", help="data visualizer",
+                        default="stocker.SRV.plotters.volume_price_plotter.VolumePricePlotter")
 
     options = parser.parse_args()
     return options
 
+
 @basic_program_structure.run_function()
 def main(options):
-    Visualizer.main(stream_file, data_visualizer)
+    Visualizer.main(options.stream_file, options.data_visualizer)
+
 
 if __name__ == "__main__":
     main()
